2b898d75595432bebed73560ab91cc358a66ebdc
Mauricio Munoz <j.mauricio.munoz1@gmail.com>
2023-09-26 12:23:14 -0700

    Changed func

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const dayOfTheWeek = (date = new Date()) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    console.log(days)
-    return days[date.getDay()];
+    return days[date.getDay() - 1];
 }
 
 
